ef09e02cd6a6595e18422742e973518e147a0da5
Brian Cassidy <brian.cassidy@gmail.com>
2014-08-25 14:14:51 -0300

    fix js

--- library_hours/js/daterange.js
+++ library_hours/js/daterange.js
@@ -1,27 +1,25 @@
 var DateRange = {};
 
-if (Drupal.jsEnabled) {
-	$(document).ready(function() {
-		$('.daterange-view-link').click(function() {
-			DateRange.getView($(this));
-			return false;
-		});
+jQuery(document).ready(function() {
+	jQuery('.daterange-view-link').click(function() {
+		DateRange.getView(jQuery(this));
+		return false;
 	});
-	
-	DateRange.getView = function(element) {
-		var url = element.attr('href');
-		if (element.attr('hasView')) {
-			element.text('View');
-			element.attr('hasView', '');
-			element.parents().filter('tr:first').next('tr').slideUp('slow').remove();
-		}
-		else {
-			$.post(Drupal.settings.daterange.view_url, {url: url}, function(data) {
-				if (data == '') return;
-				element.parents().filter('tr:first').after(data).slideDown('slow');
-				element.text('Hide');
-				element.attr('hasView', true);
-			}, 'html');
-		}
+});
+
+DateRange.getView = function(element) {
+	var url = element.attr('href');
+	if (element.attr('hasView')) {
+		element.text('View');
+		element.attr('hasView', '');
+		element.parents().filter('tr:first').next('tr').slideUp('slow').remove();
+	}
+	else {
+		jQuery.post(Drupal.settings.daterange.view_url, {url: url}, function(data) {
+			if (data == '') return;
+			element.parents().filter('tr:first').after(data).slideDown('slow');
+			element.text('Hide');
+			element.attr('hasView', true);
+		}, 'html');
 	}
 }
